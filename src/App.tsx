7211dde160dbe65fae33320e638b506c6af19cae
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-01 22:15:17 +0900

    feat: 구글 지도 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useEffect, useRef, useState } from "react";
 
-import './App.css';
+function MyMapComponent({ minHeight }: { minHeight: string }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map>();
+
+  useEffect(() => {
+    const initialCenter = {
+      lat: 37.5056102333107,
+      lng: 127.05081496722168,
+    };
+
+    const initialZoomSize = 14;
+
+    if (ref.current) {
+      const googleMap = new window.google.maps.Map(ref.current, {
+        center: initialCenter,
+        zoom: initialZoomSize,
+      });
+      setMap(googleMap);
+    }
+  }, []);
+
+  return <div ref={ref} id="map" style={{ minHeight: minHeight, }} />;
+}
+
+const render = (status: Status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <>로딩중...</>;
+    case Status.FAILURE:
+      return <>에러 발생</>;
+    case Status.SUCCESS:
+      return <></>;
+  }
+};
 
 function App() {
-  return (
-    <div >
 
-    </div>
+  return (
+    <Wrapper apiKey={`${process.env.REACT_APP_API_KEY}`} render={render} >
+      <MyMapComponent minHeight="100vh" />
+    </Wrapper>
   );
 }
 
