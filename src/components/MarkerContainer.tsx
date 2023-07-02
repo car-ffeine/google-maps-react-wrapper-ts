75af3bf607bdc6f12561599747520cb19e9c6a9d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-02 20:46:45 +0900

    feat: 마커 아무 위치에 찍을 수 있는 기능 구현

--- src/components/MarkerContainer.tsx
+++ src/components/MarkerContainer.tsx
@@ -1,11 +1,31 @@
 import { googleMapStore } from "../store/googleMapStore";
 import { useExternalValue } from "../utils/x-state";
+import {useEffect} from "react";
+import {markers} from "../data/markers";
 
 function MarkerContainer() {
   const googleMap = useExternalValue(googleMapStore);
 
+
+  const getMarkers = () => {
+    console.log('markers!')
+    markers.forEach((marker)=>{
+      return new google.maps.Marker({
+        position: {lat: marker.lat, lng: marker.lng},
+        map: googleMap,
+        title: `${marker.title}`,
+      });
+    })
+  }
+
+
+  useEffect(()=>{
+    getMarkers();
+  },[])
+
   return (
     <>
+
     </>
   )
 }
--- src/data/markers.ts
+++ src/data/markers.ts
@@ -18,4 +18,40 @@ export const markers: Marker[] = [
     lng: 127.0,
     title: 'yay1'
   },
+  {
+    id: 2,
+    lat: 37.5,
+    lng: 127.1,
+    title: 'yay2'
+  },
+  {
+    id: 3,
+    lat: 37.6,
+    lng: 127.0,
+    title: 'yay3'
+  },
+  {
+    id: 4,
+    lat: 37.55,
+    lng: 127.05,
+    title: 'yay0'
+  },
+  {
+    id: 5,
+    lat: 37.55,
+    lng: 127.1,
+    title: 'yay1'
+  },
+  {
+    id: 6,
+    lat: 37.66,
+    lng: 127.15,
+    title: 'yay2'
+  },
+  {
+    id: 7,
+    lat: 37.77,
+    lng: 127.33,
+    title: 'yay3'
+  },
 ]
