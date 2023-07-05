20c557125cb61608f6954de67c7c3d93673d8e93
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-06 00:27:13 +0900

    feat: fetching 상태 통제하는 기능 구현

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -5,6 +5,14 @@ function UI() {
   // react-query
   const { isFetching, ...queryInfo } = useMarkers();
 
+  if (isFetching) {
+    return (
+      <div style={{ zIndex: 999, position: 'fixed', bottom: 10, right: 10, backgroundColor: 'white', padding: 10 }}>
+        ⌛
+      </div>
+    )
+  }
+
   return (
     <>
       {
