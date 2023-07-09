fc6c337791285fa52fdecf47e2827a6097ab05bc
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-09 22:53:26 +0900

    refactor: 현재 위치 및 델타 값 알아내는 로직 개선

--- src/query/markerQuery.ts
+++ src/query/markerQuery.ts
@@ -3,24 +3,33 @@ import { Marker, MarkersRequest } from "../types/type";
 import { useQuery } from "@tanstack/react-query";
 import { googleMapStore } from "../store/googleMapStore";
 
-export async function fetchMarkers(): Promise<Marker[]> {
-  const map = googleMapStore.getState();
-  const center = map?.getCenter() as google.maps.LatLng;
-  const bounds = map?.getBounds() as google.maps.LatLngBounds;
+const getDisplayPosition = (map:google.maps.Map) => {
+  const center = map.getCenter() as google.maps.LatLng;
+  const bounds = map.getBounds() as google.maps.LatLngBounds;
 
-  const deltaX = (bounds?.getNorthEast().lng() - bounds?.getSouthWest().lng()) / 2;
-  const deltaY = (bounds?.getNorthEast().lat() - bounds?.getSouthWest().lat()) / 2;
+  const deltaX = (bounds.getNorthEast().lng() - bounds.getSouthWest().lng()) / 2;
+  const deltaY = (bounds.getNorthEast().lat() - bounds.getSouthWest().lat()) / 2;
   const centerX = center.lng();
   const centerY = center.lat();
-  const query = {
+
+  return {
     lng: centerX,
     lat: centerY,
     deltaX,
     deltaY
   } as MarkersRequest
+}
 
-  const res = await axios.post('/getMarkers', query);
-  return res.data;
+export async function fetchMarkers(): Promise<Marker[]> {
+  const map = googleMapStore.getState();
+  if(map){
+    const query = getDisplayPosition(map);
+    const res = await axios.post('/getMarkers', query);
+    return res.data;
+  }
+  else {
+    return [];
+  }
 }
 
 export function useMarkers() {
